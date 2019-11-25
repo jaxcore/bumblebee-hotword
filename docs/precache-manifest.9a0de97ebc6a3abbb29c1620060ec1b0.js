8f07b6ee699ef7702420d35659bdc92d2634a86a
Dan Steinman <dansteinman@gmail.com>
2019-11-25 10:08:25 -0500

    update docs

---
+++ docs/precache-manifest.9a0de97ebc6a3abbb29c1620060ec1b0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96ac930644bfa8d9191304fd41b98edf",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c0fe4674a7c99a97377f",
+    "url": "./static/css/main.dbd2b449.chunk.css"
+  },
+  {
+    "revision": "5e9993f89a4e7d9d3b67",
+    "url": "./static/js/2.11509cec.chunk.js"
+  },
+  {
+    "revision": "c0fe4674a7c99a97377f",
+    "url": "./static/js/main.5f26d6c4.chunk.js"
+  },
+  {
+    "revision": "662e30476bab91ffd584",
+    "url": "./static/js/runtime-main.659e08b0.js"
+  }
+]);
